2a9d4e7a44469daf53f9fdbec45eee783e7b7149
Supercolbat <supercolbat@gmail.com>
2020-10-15 20:52:14 +0000

    removed file preview

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-from flask import Flask, render_template, url_for, redirect, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
+from os import listdir
+from os.path import isfile, join
 
 app = Flask('app')
 
 @app.route('/')
 def index():
-    with open("files.txt") as f:
-        return '<br>'.join(f.readlines())
+    return "<h1>0x0000 file hosting</h1>"
+    # return '<br>'.join([f for f in listdir('files') if isfile(join('files', f))])
 
-@app.route('/cat.png')
-def cat():
-    print("RICK ROLLED")
-    return redirect(url_for('files', filename='cat.mp4'))
-    #return send_from_directory('files', 'cat.mp4')
-
-@app.route('/dog.png')
-def dog():
-    return send_from_directory('files', 'dog.png')
-
-@app.route('/mspaint.mp4')
-def mspaint():
-    return send_from_directory('files', 'mspaint.mp4')
-
-@app.route('/roux.html')
-def freerobux():
-    return render_template('roux.html')
+@app.route('/<file>')
+def returnfile(file):
+    try:
+        return send_from_directory('files', file)
+    except:
+        return f"File '{file}'not found"
 
 app.run(host='0.0.0.0', port=8080)
